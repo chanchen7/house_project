08af300622edfa78e5f4d5a6cbc831a8cb86c877
chanchen7 <chanchen@qq.com>
2018-10-23 22:11:36 +0800

    测试githun结束
    Signed-off-by: chanchen7 <chanchen@qq.com>

--- house_service/app.js
+++ house_service/app.js
@@ -10,8 +10,6 @@ var apiRouter = require('./routes/api')
 
 var app = express();
 
-//这里是测试github的注释，20181023
-
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-Width, yourHeardFeild');
